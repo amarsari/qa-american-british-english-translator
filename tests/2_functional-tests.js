888d70297980983fa94bf0ae6804d2197ca8d0f3
Antonio Marsari <antonio.marsari@gmail.com>
2025-04-01 10:39:03 -0300

    test: add functional tests for translation API endpoints DONE!

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,7 +6,97 @@ const server = require('../server.js');
 chai.use(chaiHttp);
 
 let Translator = require('../components/translator.js');
+let translator = new Translator();
 
 suite('Functional Tests', () => {
-
+    suite('Test different POST requests.', () => {
+        //Translation with text and locale fields: POST request to /api/translate
+        test("Translation with text and locale fields: POST request to /api/translate", (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit.',
+                    locale: 'american-to-british'
+                })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.translation, 'Mangoes are my <span class="highlight">favourite</span> fruit.');
+                });
+                done();
+        });
+        //Translation with text and invalid locale field: POST request to /api/translate
+        test("Translation with text and invalid locale field: POST request to /api/translate", (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit.',
+                    locale: 'american'
+                })
+                .end((_, res) => {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, 'Invalid value for locale field' );
+                });
+                done();
+        });
+        //Translation with missing text field: POST request to /api/translate
+        test("Translation with missing text field: POST request to /api/translate", (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    locale: 'american-to-british'
+                })
+                .end((_, res) => {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, 'Required field(s) missing');
+                });
+                done();
+        });
+        //Translation with missing locale field: POST request to /api/translate
+        test("Translation with missing locale field: POST request to /api/translate", (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit.'
+                })
+                .end((_, res) => {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, 'Required field(s) missing');
+                });
+                done();
+        });
+        //Translation with empty text: POST request to /api/translate
+        test("Translation with empty text: POST request to /api/translate", (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: '',
+                    locale: 'american-to-british'
+                })
+                .end((_, res) => {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.error, 'No text to translate');
+                });
+                done();
+        });
+        //Translation with text that needs no translation: POST request to /api/translate
+        test("Translation with text that needs no translation: POST request to /api/translate", (done) => {
+            chai
+                .request(server)
+                .post('/api/translate')
+                .send({
+                    text: 'Mangoes are my favorite fruit.',
+                    locale: 'british-to-american'
+                })
+                .end((_, res) => {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.translation, 'Everything looks good to me');
+                });
+                done();
+        });
+    });
 });
